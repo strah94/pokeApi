b0b3303f44cf1c54ab8a888fae7b552c0965ef2d
strah94 <strahinjavelickovic94@gmail.com>
2021-07-07 00:26:35 +0200

    Added route to Homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+};
 
 export default App;
